47407e59fe430f1a86e5fb47d9d4c37b68434f4a
Brian Malehorn <brian@lumos.com>
2025-10-12 14:53:21 -0700

    add vscode-tmgrammar-test for testing tmlanguage changes

---
+++ tests/grammar/basic.test.fish
@@ -0,0 +1,6 @@
+# SYNTAX TEST "source.fish" "Fish shell syntax highlighting - commands"
+
+# See https://www.npmjs.com/package/vscode-tmgrammar-test for rules.
+
+echo hello
+#^^^ support.function.command.fish

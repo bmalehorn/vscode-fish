28a9363b3c418384ca4480dac4e2d2dda53f8c6b
Brian Malehorn <brian@lumos.com>
2025-10-15 19:02:44 -0700

    1.0.47: remove meta.embedded.slice.fish
    It's not really a sub-language.
    
    Really, what happens between the [ ] is the same as what happens outside of them: `$(subcommand)`` or `(subcommand)` are expended depending on if the list variable is already in a string.
    
    `..` is not a low-binding keyword. What happens is: the string passed into the [ ] is interpreted by an internal fish function, so something like `$list_variable[$(echo '1..2')]` is valid and the same as `$list_variable[1..2]`. To bollow from Python, it's more like a custom `__getitem__` implementation that has its own meaning of `..`, than a syntax construct.
    
    However, the fish shell itself highlights the stuff between [ ] differently, so maybe I will do this one day. A meta language that highlights everything inside as a variable. If coming from a double quoted string, include only variables, if coming from something else, include all usual language constructs.

--- tests/grammar/escape.test.fish
+++ tests/grammar/escape.test.fish
@@ -13,10 +13,6 @@ foo (ack \) bar)
 #   ^^^^^^^^^^^^ meta.embedded.subshell.fish
 #        ^^ constant.character.escape.single.fish
 
-# slice is not fooled by \]
-echo $list_variable[1 \] 2]
-#                  ^^^^^^^^ meta.embedded.slice.fish
-#                     ^^ constant.character.escape.single.fish
 
 # subshell is not fooled by \) in a string
 foo "$(ack \) bar)"
@@ -29,8 +25,3 @@ foo (ack \\) bar
 #        ^^ constant.character.escape.single.fish
 #            ^^^ - meta.embedded.subshell.fish
 
-# double backslash ends slice (backslash is escaped, not bracket)
-echo $list_variable[1 \\] 2
-#                  ^^^^^ meta.embedded.slice.fish
-#                     ^^ constant.character.escape.single.fish
-#                         ^ - meta.embedded.slice.fish
--- tests/grammar/list-variables.test.fish
+++ tests/grammar/list-variables.test.fish
@@ -3,30 +3,16 @@
 echo $list_variable[1]
 #    ^ variable.other.normal.fish punctuation.definition.variable.fish
 #     ^^^^^^^^^^^^^ variable.other.normal.fish
-#                   ^ meta.embedded.slice.fish
-
-# In a meta.embedded.slice.fish, `..` is treated as a keyword
-echo $list_variable[1..-2]
-#    ^ variable.other.normal.fish punctuation.definition.variable.fish
-#     ^^^^^^^^^^^^^ variable.other.normal.fish
-#                   ^^^^^ meta.embedded.slice.fish
-#                    ^^ keyword.control.fish
-
-# not a keyword outside of a list variable context
-echo 1..-2
-#     ^^ - keyword.control.fish
 
 # This applies in a substring too, similar to subshell
 echo "$list_variable[1] 2"
 #      ^^^^^^^^^^^^^ string.quoted.double.fish variable.other.normal.fish
-#                   ^^^ meta.embedded.slice.fish
 #                   ^^^^^ string.quoted.double.fish
 
-# subshells are supposed inside slices
+# subshells are supported inside slices
 echo $list_variable[(math 1 + 2)..3]
 #     ^^^^^^^^^^^^^ variable.other.normal.fish
 #                   ^^^^^^^^^^^^ meta.embedded.subshell.fish
-#                    ^^^^ support.function.command.fish
 
 # nesting strings, slice, subshell, all works
 # $() makes a subshell in a string, including in a slice.
@@ -40,13 +26,16 @@ echo "foo $list_variable[1..(math 1 + 1)]"
 
 # however it does take effect outside of strings.
 echo $list_variable[1..(math 1 + 1)]
-#                       ^^^^^^^^^^^^ meta.embedded.subshell.fish
+#                      ^^^^^^^^^^^^ meta.embedded.subshell.fish
 
 # The start of a slice is not the same as the start of a line or subshell.
-# It should not cause the first work to be highlighted as a command.
+# It should not cause the first word to be highlighted as a command.
 echo $list_variable[echo hello]
-#                   ^^^^ - support.function.command.fish
 
-# No comments in multiline list variables - # is interpreted as a value.
-echo $list_variable[1..#]
-#                      ^^ - punctuation.definition.comment.fish
+# Valid
+echo "$list_variable[$(echo '1..2')]"
+#                    ^^^^^^^^^^^^^^ meta.embedded.subshell.fish
+
+# Not valid at runtime: 'fish: Invalid index value'
+echo "$list_variable[(echo '1..2')]"
+#                    ^^^^^^^^^^^^^ - meta.embedded.subshell.fish
